7754a0ddbaf16f2ce3e080c4af8b0a7ac9654673
Adam Bezecny (DHL IT Services) <Adam.Bezecny@dhl.com>
2020-02-27 21:26:05 +0100

    single threaded server implemented

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::io::prelude::*;
+use std::net::TcpStream;
+use std::net::TcpListener;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+
+    let get = b"GET / HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n{}", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\nContent-Type: text/html\r\n\r\n", "404.html")
+    };    
+    
+    let contents = fs::read_to_string(filename).unwrap();
+
+    let response = format!("{}{}", status_line, contents);
+    
+    println!("sending response:\r\n\r\n{}\r\n", response);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();    
 }
