3f8332332a61c2ec593b76cf0ba1a86e363e9c0e
Adam Bezecny (DHL IT Services) <Adam.Bezecny@dhl.com>
2020-03-01 14:20:33 +0100

    ctrl+c handler implemented

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,3 +1,4 @@
+use ctrlc;
 use std::io::prelude::*;
 use std::net::TcpStream;
 use std::net::TcpListener;
@@ -5,19 +6,66 @@ use std::fs;
 use std::thread;
 use std::time::Duration;
 use hello::ThreadPool;
+use std::sync::Arc;
+use std::sync::atomic::{AtomicBool, Ordering};
+// use std::io;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    //  https://stackoverflow.com/questions/56692961/graceful-exit-tcplistener-incoming
+    // listener.set_nonblocking(true).expect("Cannot set non-blocking");
     let pool = ThreadPool::new(4);
+    let running = Arc::new(AtomicBool::new(true));
+    let r = running.clone();
 
-    for stream in listener.incoming().take(2) {
+    ctrlc::set_handler(move || {
+        println!("Shutting down...");
+        r.store(false, Ordering::SeqCst);
+    }).expect("Error setting Ctrl-C handler");    
+    
+    // non-blocking polling not working properly for now
+    /* for stream in listener.incoming() {
+
+        let still_running =  running.load(Ordering::SeqCst);
+        if still_running == false {
+            break;
+        }
+        
+        match stream {
+            Ok(s) => {
+                pool.execute(|| {
+                    handle_connection(s);
+                });
+            }
+            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+                 println!("WouldBlock error: {}", e);
+                thread::sleep(Duration::from_secs(1));
+                //
+                // would throw A non-blocking socket operation could not be completed immediately. (os error 10035)
+                // see https://github.com/tokio-rs/mio/issues/727
+                // this is normal for non blocking socket operations when no data is available
+                // let's just continue
+                continue;
+            }
+            Err(e) => panic!("encountered IO error: {}", e),
+        }        
+    } */
+
+    for stream in listener.incoming() {
+        // this will shutdown only after accepting new connection, incoming() call block until new connection
+        // working this around is pretty complex for now :)
+        let still_running =  running.load(Ordering::SeqCst);
+        if still_running == false {
+            break;
+        }
+
         let stream = stream.unwrap();
 
         pool.execute(|| {
             handle_connection(stream);
         });
-    }
-    println!("Shutting down.");
+    }    
+    
 }
 
 fn handle_connection(mut stream: TcpStream) {
